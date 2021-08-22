bb945a8842132bbaad61f174b1448d826858e9de
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-08-22 13:05:27 +0200

    AddData

---
+++ Urania/Urania.Core/Data/Swg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace Urania.Core.Data {
+    public class Swg {
+        public Dictionary<SwgName, decimal> Values = new Dictionary<SwgName, decimal> {
+            [SwgName.Ten] = 3.25M,
+            [SwgName.Eleven] = 2.95M,
+            [SwgName.Twelve] = 2.64M,
+            [SwgName.Thirteen] = 2.34M,
+            [SwgName.Fourteen] = 2.03M,
+            [SwgName.Fifteen] = 1.83M,
+            [SwgName.Sixteen] = 1.63M,
+            [SwgName.Seventeen] = 1.42M,
+            [SwgName.Eighteen] = 1.22M,
+            [SwgName.Nineteen] = 1.02M,
+            [SwgName.Twenty] = 0.91M,
+            [SwgName.TwentyOne] = 0.81M,
+            [SwgName.TwentyTwo] = 0.71M,
+            [SwgName.TwentyThree] = 0.61M,
+            [SwgName.TwentyFour] = 0.56M,
+            [SwgName.TwentyFive] = 0.51M,
+            [SwgName.TwentySix] = 0.46M,
+            [SwgName.TwentySeven] = 0.42M,
+            [SwgName.TwentyEight] = 0.38M,
+            [SwgName.TwentyNine] = 0.35M,
+            [SwgName.Thirty] = 0.32M,
+            [SwgName.ThirtyOne] = 0.30M,
+            [SwgName.ThirtyTwo] = 0.27M,
+            [SwgName.ThirtyThree] = 0.25M,
+            [SwgName.ThirtyFour] = 0.23M,
+            [SwgName.ThirtyFive] = 0.21M,
+            [SwgName.ThirtySix] = 0.19M,
+            [SwgName.ThirtySeven] = 0.17M,
+            [SwgName.ThirtyEight] = 0.15M,
+            [SwgName.ThirtyNine] = 0.13M,
+            [SwgName.Forty] = 0.12M
+        };
+    }
+}
