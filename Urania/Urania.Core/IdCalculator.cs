d90a151e469a974ac04c6c8775692edc8f50d145
Katarzyna-Kadziolka <62292047+Katarzyna-Kadziolka@users.noreply.github.com>
2021-08-31 16:00:24 +0200

    Calculator and tests (#5)

---
+++ Urania/Urania.Core/IdCalculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Urania.Core {
+    public class IdCalculator {
+        public static decimal Calculate(decimal? ar, decimal? wd, decimal? od) {
+            if (ar != null && wd != null) {
+                return (decimal) (ar * wd);
+            }
+
+            if (ar != null && od != null) {
+                if (ar == -2) {
+                    throw new DivideByZeroException($"{nameof(ar)} cannot be equal -2");
+                }
+                return (decimal) (ar * od / (2 + ar));
+            }
+
+            if (wd != null && od != null) {
+                return (decimal) (od - 2 * wd);
+            }
+
+            throw new ArgumentNullException
+                (string.Empty, $"Values: {nameof(ar)} = {ar}, {nameof(wd)} = {wd}, {nameof(od)} = {od}");
+        }
+    }
+}
---
+++ Urania/Urania.Tests/IdCalculatorTests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Urania.Core;
+
+namespace Urania.Tests {
+    public class IdCalculatorTests {
+        [Test]
+        public void Calculate_AllArgumentsAreDecimals_ShouldReturnDecimal() {
+            //Arrange
+            var ar = 5.31M;
+            var wd = 6.99M;
+            var od = 51.0969M;
+            var expectingResult = ar * wd;
+            
+            //Act
+            var result = IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            result.Should().Be(expectingResult);
+        }
+
+        [Test]
+        public void Calculate_OdIsNull_ShouldReturnDecimal() {
+            //Arrange
+            var ar = 5.31M;
+            var wd = 6.99M;
+            decimal? od = null;
+            var expectingResult = ar * wd;
+
+            //Act
+            var result = IdCalculator.Calculate(ar, wd, od);
+
+            //Assert
+            result.Should().Be(expectingResult);
+        }
+        [Test]
+        public void Calculate_WdIsNull_ShouldReturnDecimal() {
+            //Arrange
+            var ar = 5.31M;
+            decimal? wd = null;
+            var od = 51.0969M;
+            var expectingResult = ar * od / (2 + ar);
+
+            //Act
+            var result = IdCalculator.Calculate(ar, wd, od);
+
+            //Assert
+            result.Should().Be(expectingResult);
+        }
+        [Test]
+        public void Calculate_ArIsNull_ShouldReturnDecimal() {
+            //Arrange
+            decimal? ar = null;
+            var wd = 6.99M;
+            var od = 51.0969M;
+            var expectingResult = od - 2 * wd;
+            
+            //Act
+            var result = IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            result.Should().Be(expectingResult);
+        }
+        [Test]
+        public void Calculate_ArAndWdAreNulls_ShouldThrowArgumentNullException() {
+            //Arrange
+            decimal? ar = null;
+            decimal? wd = null;
+            var od = 51.0969M;
+            
+            //Act
+            Action act = () => IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            act.Should().Throw<ArgumentNullException>
+                (string.Empty, $"Values: {nameof(ar)} = {ar}, {nameof(wd)} = {wd}, {nameof(od)} = {od}");
+        }
+        [Test]
+        public void Calculate_ArAndOdAreNulls_ShouldThrowArgumentNullException() {
+            //Arrange
+            decimal? ar = null;
+            decimal? wd = 6.99M;
+            decimal? od = null;
+            
+            //Act
+            Action act = () => IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            act.Should().Throw<ArgumentNullException>
+                (string.Empty, $"Values: {nameof(ar)} = {ar}, {nameof(wd)} = {wd}, {nameof(od)} = {od}");
+        }
+        [Test]
+        public void Calculate_OdAndWdAreNulls_ShouldThrowArgumentNullException() {
+            //Arrange
+            decimal? ar = 5.31M;
+            decimal? wd = null;
+            decimal? od = null;
+            
+            //Act
+            Action act = () => IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            act.Should().Throw<ArgumentNullException>
+                (string.Empty, $"Values: {nameof(ar)} = {ar}, {nameof(wd)} = {wd}, {nameof(od)} = {od}");
+        }
+        [Test]
+        public void Calculate_AllArgumentsAreNulls_ShouldThrowArgumentNullException() {
+            //Arrange
+            decimal? ar = null;
+            decimal? wd = null;
+            decimal? od = null;
+            
+            //Act
+            Action act = () => IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            act.Should().Throw<ArgumentNullException>
+                (string.Empty, $"Values: {nameof(ar)} = {ar}, {nameof(wd)} = {wd}, {nameof(od)} = {od}");
+        }
+        [Test]
+        public void Calculate_ArIsEqualMinusTwoAndWdIsNull_ShouldThrowDivideByZeroException() {
+            //Arrange
+            var ar = -2M;
+            decimal? wd = null;
+            var od = 51.0969M;
+
+            //Act
+            Action act = () => IdCalculator.Calculate(ar, wd, od);
+            
+            //Assert
+            act.Should().Throw<DivideByZeroException>($"{nameof(ar)} cannot be equal -2");
+        }
+    }
+}
