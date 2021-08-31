79bc8753e26d186d15604518441053807d99af5d
Katarzyna-Kadziolka <62292047+Katarzyna-Kadziolka@users.noreply.github.com>
2021-08-31 16:00:37 +0200

    Feature/counting ar (#4)
    * Add method sygnature
    
    * Add ArCalculator and tests for it
    
    * nie dokonczone
    
    * Add last test
    
    * Little fixes
    
    * Remove nulls

---
+++ Urania/Urania.Tests/ArCalculatorTests.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using Urania.Core;
+
+namespace Urania.Tests {
+	public class ArCalculatorTests {
+		[Test]
+		public void Calculate_AllArgumentsAreDecimals_ShouldReturnDecimalWithTwoNumbersAfterDot() {
+			//Arrange
+			var id = 7.77M;
+			var wd = 13.13M;
+			var od = 34.03M;
+			var expectedResult = id / wd;
+
+			//Act
+			var result = ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			result.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void Calculate_IdIsNull_ShouldReturnDecimalWithTwoNumbersAfterDot() {
+			//Arrange
+			decimal? id = null;
+			var wd = 13.13M;
+			var od = 34.03M;
+			var expectedResult = (od - 2 * wd) / wd;
+
+			//Act
+			var result = ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			result.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void Calculate_WdIsNull_ShouldReturnDecimalWithTwoNumbersAfterDot() {
+			//Arrange
+			var id = 7.77M;
+			var od = 34.03M;
+			decimal? wd = null;
+			var expectedResult = id / ((od - id) / 2);
+
+			//Act
+			var result = ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			result.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void Calculate_OdIsNull_ShouldReturnDecimalWithTwoNumbersAfterDot() {
+			//Arrange
+			var id = 7.77M;
+			var wd = 13.13M;
+			decimal? od = null;
+			var expectedResult = id / wd;
+
+			//Act
+			var result = ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			result.Should().Be(expectedResult);
+		}
+
+		[Test]
+		public void Calculate_IdAndWdAreNulls_ShouldThrowArgumentNullException() {
+			//Arrange
+			decimal? id = null;
+			decimal? wd = null;
+			var od = 34.03M;
+
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<ArgumentNullException>
+				(string.Empty, $"Values: {nameof(id)} == {id}, {nameof(wd)} == {wd}, {nameof(od)} == {od}");
+		}
+
+		[Test]
+		public void Calculate_IdAndOdAreNulls_ShouldThrowArgumentNullException() {
+			//Arrange
+			decimal? id = null;
+			decimal? od = null;
+			var wd = 13.13M;
+
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<ArgumentNullException>
+				(string.Empty, $"Values: {nameof(id)} == {id}, {nameof(wd)} == {wd}, {nameof(od)} == {od}");
+		}
+
+		[Test]
+		public void Calculate_OdAndWdAreNulls_ShouldThrowArgumentNullException() {
+			//Arrange
+			decimal? od = null;
+			decimal? wd = null;
+			var id = 7.77M;
+
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<ArgumentNullException>
+				(string.Empty, $"Values: {nameof(id)} == {id}, {nameof(wd)} == {wd}, {nameof(od)} == {od}");
+		}
+
+		[Test]
+		public void Calculate_AllArgumentsAreNulls_ShouldThrowArgumentNullException() {
+			//Arrange
+			decimal? od = null;
+			decimal? wd = null;
+			decimal? id = null;
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<ArgumentNullException>
+				(string.Empty, $"Values: {nameof(id)} == {id}, {nameof(wd)} == {wd}, {nameof(od)} == {od}");
+		}
+		[Test]
+		public void Calculate_WdIsEqualZeroIdIsEqualNull_ShouldThrowDivideByZeroException() {
+			//Arrange
+			decimal? od = 34.03M;
+			decimal? wd = 0M;
+			decimal? id = null;
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<DivideByZeroException>($"{nameof(wd)} cannot be 0");
+		}
+		[Test]
+		public void Calculate_WdIsEqualZeroOdIsEqualNull_ShouldThrowDivideByZeroException() {
+			//Arrange
+			decimal? od = null;
+			decimal? wd = 0M;
+			decimal? id = 7.77M;
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<DivideByZeroException>($"{nameof(wd)} cannot be 0");
+		}
+		[Test]
+		public void Calculate_OdEqualIdAndWdIsEqualNull_ShouldThrowDivideByZeroException() {
+			//Arrange
+			decimal? od = 7.77M;
+			decimal? wd = null;
+			decimal? id = 7.77M;
+			//Act
+			Action act = () => ArCalculator.Calculate(id, wd, od);
+
+			//Assert
+			act.Should().Throw<DivideByZeroException>($"{nameof(od)} cannot be equal {nameof(id)}");
+		}
+	}
+}
