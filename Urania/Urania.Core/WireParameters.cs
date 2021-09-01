926bdb47e3bc4ded0684e7d039437152cd0f1e95
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-09-01 19:19:46 +0200

    Created class WireParameters

---
+++ Urania/Urania.Core/WireParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace Urania.Core {
+    internal class WireParameters {
+        public int Wd { get; set; }
+        public int Id { get; set; }
+        public int Od { get; set; }
+        public int Ar { get; set; }
+    }
+}
