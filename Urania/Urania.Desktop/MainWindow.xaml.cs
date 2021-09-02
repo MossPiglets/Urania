44488453a53dad61d64908d30c916d1dd664cad5
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-09-02 19:52:16 +0200

    COnverter and binding

--- Urania/Urania.Desktop/MainWindow.xaml.cs
+++ Urania/Urania.Desktop/MainWindow.xaml.cs
@@ -1,11 +1,19 @@
-﻿namespace Urania.Desktop
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow {
-        public MainWindow() {
-            InitializeComponent();
-        }
-    }
+﻿using System.Windows;
+using Urania.Desktop.States;
+
+namespace Urania.Desktop {
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow {
+		public WdState WdState { get; set; }
+		public MainWindow() {
+			InitializeComponent();
+			this.DataContext = this;
+		}
+
+		private void ToggleButton_OnChecked(object sender, RoutedEventArgs e) {
+			MessageBox.Show(string.Format($"wdState: {WdState}"));
+		}
+	}
 }
