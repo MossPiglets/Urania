8de6a6ea4bd3f73a99e42a702db2e255e90ec923
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-09-03 18:12:32 +0200

    Now it works

--- Urania/Urania.Desktop/MainWindow.xaml.cs
+++ Urania/Urania.Desktop/MainWindow.xaml.cs
@@ -1,16 +1,15 @@
-﻿namespace Urania.Desktop
+﻿using Urania.Core;
+
+namespace Urania.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
+        public WireParameters WireParameters { get; set; } = new WireParameters();
         public MainWindow() {
             InitializeComponent();
             this.DataContext = this;
-        }
-
-        private void Button_Click(object sender, System.Windows.RoutedEventArgs e) {
-
         }
     }
 }
