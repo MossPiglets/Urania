9478322fc21aba55c750f62dab139b0a93f92994
Krystyna-Szybalska <74852815+Krystyna-Szybalska@users.noreply.github.com>
2021-09-04 12:20:11 +0200

    Feature/wire parameters (#15)
    * Layout according to project
    
    * Created class WireParameters
    
    * FIxed error
    
    * Added binding but it doesn't seem to work
    
    * Now it works
    
    * Changed int to decimal in WireParameters
    
    Co-authored-by: Morasiu <moras2601@wp.pl>

---
+++ Urania/Urania.Core/WireParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace Urania.Core {
+    public class WireParameters {
+        public decimal Wd { get; set; }
+        public decimal Id { get; set; }
+        public decimal Od { get; set; }
+        public decimal Ar { get; set; }
+    }
+}
--- Urania/Urania.Desktop/MainWindow.xaml.cs
+++ Urania/Urania.Desktop/MainWindow.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace Urania.Desktop
+﻿using Urania.Core;
+
+namespace Urania.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
+        public WireParameters WireParameters { get; set; } = new WireParameters();
         public MainWindow() {
             InitializeComponent();
+            this.DataContext = this;
         }
     }
 }
