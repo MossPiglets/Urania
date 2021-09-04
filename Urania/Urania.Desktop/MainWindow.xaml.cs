bf9c6f8aa83bb0b4c05c28d85cc923310686d97b
Katarzyna-Kadziolka <62292047+Katarzyna-Kadziolka@users.noreply.github.com>
2021-09-04 20:03:46 +0200

    Feature/state machine for wd (#13)
    * States
    
    * Converter and binding
    
    * COnverter and binding
    
    * It's working
    
    * default value
    
    * fix

--- Urania/Urania.Desktop/MainWindow.xaml.cs
+++ Urania/Urania.Desktop/MainWindow.xaml.cs
@@ -1,12 +1,14 @@
-﻿using Urania.Core;
+﻿using Urania.Desktop.States;
+using Urania.Core;
 
-namespace Urania.Desktop
-{
+namespace Urania.Desktop {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
         public WireParameters WireParameters { get; set; } = new WireParameters();
+        public WdState WdState { get; set; } = WdState.Inch;
+
         public MainWindow() {
             InitializeComponent();
             this.DataContext = this;
