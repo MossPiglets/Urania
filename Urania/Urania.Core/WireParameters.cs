f93915f3c454dcdefcc7d4a3c9c7347cd2c868a2
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-09-06 19:51:41 +0200

    Reverted commit that broke this branch :3

--- Urania/Urania.Core/WireParameters.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Urania.Core {
-    public class WireParameters {
-        public decimal Wd { get; set; }
-        public decimal Id { get; set; }
-        public decimal Od { get; set; }
-        public decimal Ar { get; set; }
-    }
-}
--- Urania/Urania.Desktop/MainWindow.xaml.cs
+++ Urania/Urania.Desktop/MainWindow.xaml.cs
@@ -1,15 +1,11 @@
-﻿using Urania.Core;
-
-namespace Urania.Desktop
+﻿namespace Urania.Desktop
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow {
-        public WireParameters WireParameters { get; set; } = new WireParameters();
         public MainWindow() {
             InitializeComponent();
-            this.DataContext = this;
         }
     }
 }
