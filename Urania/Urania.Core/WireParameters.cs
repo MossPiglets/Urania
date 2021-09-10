32fced93cc5910823dc4ce535cf4a2d8a45fecfc
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-09-10 16:04:37 +0200

    Changed propeties

--- Urania/Urania.Core/WireParameters.cs
+++ Urania/Urania.Core/WireParameters.cs
@@ -3,17 +3,39 @@ using System.Runtime.CompilerServices;
 
 namespace Urania.Core {
     public class WireParameters : INotifyPropertyChanged {
+
+        private decimal? _wd;
+        private decimal? _od;
+        private decimal? _id;
+        private decimal? _ar;
         public decimal? Wd {
-            set { OnPropertyChanged(); }
+            get => _wd;
+            set {
+                _wd = value;
+                OnPropertyChanged(nameof(Wd));
+            }
         }
+
         public decimal? Id {
-            set { OnPropertyChanged(); }
+            get => _id;
+            set {
+                _id = value;
+                OnPropertyChanged(nameof(Id));
+            }
         }
         public decimal? Od {
-            set { OnPropertyChanged(); }
+            get => _od;
+            set {
+                _od = value;
+                OnPropertyChanged(nameof(Od));
+            }
         }
         public decimal? Ar {
-            set { OnPropertyChanged(); }
+            get => _ar;
+            set {
+                _ar = value;
+                OnPropertyChanged(nameof(Ar));
+            }
         }
         public event PropertyChangedEventHandler PropertyChanged;
 
