c5c8b129e18762a5b380dc3c96c54346379758e2
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-09-10 11:51:02 +0200

    Deleted backup, merged with develop

--- Urania/Urania.Desktop/States/WdState.cs
+++ Urania/Urania.Desktop/States/WdState.cs
@@ -1,18 +1,8 @@
 ﻿namespace Urania.Desktop.States {
-<<<<<<< HEAD
-
-    public enum WdState {
-        Unknown = 0,
-        Inch,
-        Swg,
-        Awg
-    }
-=======
 	public enum WdState {
 		Unknown = 0,
 		Millimeter,
 		Swg,
 		Awg
 	}
->>>>>>> 20a6f88083beb87790a8654c479d70ea1f760713
 }
