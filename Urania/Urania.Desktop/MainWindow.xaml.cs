b20cb640d4231760a9081458d07cd6b9159e7fc7
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-09-22 20:03:30 +0200

    - created MainViewModel, which communicates with States and WireParameters - fixed radiobutton selection on "CleanButton_Click" - grouped comboboxes/textboxes with corresponding radiobuttons - comboboxes' reset when other radiobutton is selected

--- Urania/Urania.Desktop/MainWindow.xaml.cs
+++ Urania/Urania.Desktop/MainWindow.xaml.cs
@@ -1,61 +1,73 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
-using Urania.Desktop.States;
 using Urania.Core;
 using Urania.Core.Data;
+using Urania.Desktop.States;
 
 namespace Urania.Desktop {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow {
-        public WireParameters WireParameters { get; set; } = new WireParameters();
-        public WdState WdState { get; set; } = WdState.Millimeter;
-        public IdState IdState { get; set; } = IdState.Millimeter;
-
+        MainViewModel MainViewModel { get; set; } = new MainViewModel();
+       
         public MainWindow() {
             InitializeComponent();
-            this.DataContext = this;
+            this.DataContext = MainViewModel;
         }
-
         private void CleanButton_Click(object sender, System.Windows.RoutedEventArgs e) {
-            WireParameters.Wd = null;
-            WireParameters.Id = null;
-            WireParameters.Od = null;
-            WireParameters.Ar = null;
+            MainViewModel.WireParameters.Wd = null;
+            MainViewModel.WireParameters.Id = null;
+            MainViewModel.WireParameters.Od = null;
+            MainViewModel.WireParameters.Ar = null;
             IdCalComboBox.SelectedIndex = -1;
             WdSwgComboBox.SelectedIndex = -1;
             WdAwgComboBox.SelectedIndex = -1;
-            IdMmRadiobutton.IsChecked = true;
-            WdMmRadiobutton.IsChecked = true;
+            MainViewModel.IdState = IdState.Millimeter;
+            MainViewModel.WdState = WdState.Millimeter;
         }
 
-        private void WdSwgComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) =>
-            WireParameters.Wd = Swg.Values[(SwgName) e.AddedItems[0]];
+        private void WdSwgComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (e.AddedItems.Count != 0) {
+                MainViewModel.WireParameters.Wd = Swg.Values[(SwgName)e.AddedItems[0]];
+            }
+        }
 
-        private void IdInchComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) =>
-            WireParameters.Id = Inch.Values[(InchName) e.AddedItems[0]];
-        
-        private void WdAwgComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) =>
-            WireParameters.Wd = Awg.Values[(AwgName) e.AddedItems[0]];
+        private void IdInchComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (e.AddedItems.Count != 0) {
+                MainViewModel.WireParameters.Id = Inch.Values[(InchName)e.AddedItems[0]];
+            }
+        }
+        private void WdAwgComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (e.AddedItems.Count != 0) {
+                MainViewModel.WireParameters.Wd = Awg.Values[(AwgName)e.AddedItems[0]];
+            }
+        }
 
         private void ButtonCalculate_OnClick(object sender, RoutedEventArgs e) {
-            if (WireParameters.Wd == null) {
-                WireParameters.Wd = Math.Round(WdCalculator.Calculate(WireParameters.Id, WireParameters.Ar, WireParameters.Od), 2) ;
+            if (MainViewModel.WireParameters.Wd == null) {
+                MainViewModel.WireParameters.Wd = Math.Round(WdCalculator.Calculate(MainViewModel.WireParameters.Id, MainViewModel.WireParameters.Ar, MainViewModel.WireParameters.Od), 2) ;
             }
 
-            if (WireParameters.Ar == null) {
-                WireParameters.Ar = Math.Round(ArCalculator.Calculate(WireParameters.Id, WireParameters.Wd, WireParameters.Od), 2);
+            if (MainViewModel.WireParameters.Ar == null) {
+                MainViewModel.WireParameters.Ar = Math.Round(ArCalculator.Calculate(MainViewModel.WireParameters.Id, MainViewModel.WireParameters.Wd, MainViewModel.WireParameters.Od), 2);
             }
 
-            if (WireParameters.Id == null) {
-                WireParameters.Id = Math.Round(IdCalculator.Calculate(WireParameters.Ar, WireParameters.Wd, WireParameters.Od), 2);
+            if (MainViewModel.WireParameters.Id == null) {
+                MainViewModel.WireParameters.Id = Math.Round(IdCalculator.Calculate(MainViewModel.WireParameters.Ar, MainViewModel.WireParameters.Wd, MainViewModel.WireParameters.Od), 2);
             }
 
-            if (WireParameters.Od == null) {
-                WireParameters.Od = Math.Round(OdCalculator.Calculate(WireParameters.Ar, WireParameters.Wd, WireParameters.Id), 2);
+            if (MainViewModel.WireParameters.Od == null) {
+                MainViewModel.WireParameters.Od = Math.Round(OdCalculator.Calculate(MainViewModel.WireParameters.Ar, MainViewModel.WireParameters.Wd, MainViewModel.WireParameters.Id), 2);
             }
         }
+
+        private void IdMmRadiobutton_Checked(object sender, RoutedEventArgs e) {
+            IdCalComboBox.SelectedItem = null;
+        }
+
+        private void WdMmRadiobutton_Checked(object sender, RoutedEventArgs e) {
+            WdSwgComboBox.SelectedIndex = -1;
+            WdAwgComboBox.SelectedIndex = -1;
+        }
+
     }
 }
