51f372d3790e8e3691b6addce2b4b495a2f97947
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-10-24 16:31:46 +0200

    Add another converter.....

--- Urania/Urania.Desktop/App.xaml.cs
+++ Urania/Urania.Desktop/App.xaml.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Markup;
 
-namespace Urania.Desktop
-{
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+namespace Urania.Desktop {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application {
+		public App() {
+			FrameworkElement.LanguageProperty.OverrideMetadata(
+				typeof(FrameworkElement),
+				new FrameworkPropertyMetadata(
+					XmlLanguage.GetLanguage(CultureInfo.CurrentCulture.IetfLanguageTag)));
+		}
+	}
 }
