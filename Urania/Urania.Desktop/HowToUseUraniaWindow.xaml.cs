3ceebd2276324faa89cb8c7be2b8a3692dd90c71
KRYSIAACER\Krysia <krystyna.szybalska@gmail.com>
2021-11-03 18:27:48 +0100

    some changes in text

--- Urania/Urania.Desktop/HowToUseUraniaWindow.xaml.cs
+++ Urania/Urania.Desktop/HowToUseUraniaWindow.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
+﻿using System.IO;
 using System.Windows;
 
 namespace Urania.Desktop {
 
     public partial class HowToUseUrania {
 
-        public string firstLines = File.ReadAllText("/Resources/HowToUseUrania.txt").Take(2).ToString();
-        public string lastLines = File.ReadLines("/Resources/HowToUseUrania.txt").Skip(2).ToString();
+        private string lines = File.ReadAllText("/Resources/HowToUseUrania.txt");
 
         public HowToUseUrania() {
+            lastLines.Text = lines;
             InitializeComponent();
         }
 
